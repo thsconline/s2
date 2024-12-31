161de3d1912ecb4013fbde5c132da140ab88ebc5
dp964 <thsconline@gmail.com>
2025-01-01 00:36:29 +1100

    Update search.js

--- search.js
+++ search.js
@@ -284,7 +284,7 @@ window.onload = function() {
     var searchQuery = getUrlParameter('filter');
     if (searchQuery) {
         var searchInput = document.getElementById("search-bar");
-		if(searchQuery="new-syllabus"){searchQuery = "2019-24"}
+		if(searchQuery=="new-syllabus"){searchQuery = "2019-24"}
         searchInput.value = searchQuery;  // Set the input value to the search query from the URL
 
         filterTable();  // Trigger the table filtering function
